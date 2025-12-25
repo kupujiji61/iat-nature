598262ac7f16fa18811e00b78fd3b6a544f1d9e1
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2025-12-25 19:07:40 +0300

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -93,8 +93,9 @@ define(['questAPI'], function(Quest){
 		name: 'born_bashkortostan',
 		stem: 'Вы родились, жили или живете в Республике Башкортостан?',
 		answers: [
-			{text:'Да',  value:1},
-			{text:'Нет', value:2}
+			{text:'Проживаю в Башкортостане',  value:1},
+			{text:'Сейчас не живу, но родился / проживал в Башкортостане', value:2},
+			{text:'Ничего из перечисленного', value:3}
 		]
 	});
 	
